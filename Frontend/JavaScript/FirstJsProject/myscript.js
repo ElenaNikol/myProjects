7862e73ab4b6ed08e4fb724cb9487f5e4be74d84
ElenaNikol <elena.nikolovska98@yahoo.com>
2019-02-25 13:29:21 +0100

    first commit

---
+++ Frontend/JavaScript/FirstJsProject/myscript.js
@@ -0,0 +1,18 @@
+
+function findSum(n) {
+    var sum = 0;
+    while(n){
+        var i = n%10;
+        sum += i;
+        n/=10;
+    }
+    console.log(sum);
+}
+// 101
+//num = 101^2;
+//sum = sum + num sum = sum + 101^2;
+//102
+//num = 102^2;
+//sum = 101^2 + 102^2;
+//
+findSum(3453);
